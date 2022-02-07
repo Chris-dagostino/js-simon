dd1dcd66038ef6ed2e6db1172f7e721c81d8c4df
Chris-dagostino <dagostino.christian94@gmail.com>
2022-02-07 15:48:35 +0100

    casuali

--- main.js
+++ main.js
@@ -4,7 +4,10 @@
 
 getRandomNumber = document.getElementById('casuali').innerHTML;
 
-function getRandomNumber ( 0 , 100 ) {
+min = 0;
+max = 100;
+
+function getRandomNumber ( min , max ) {
     return Math.floor(Math.random()*(max - min +1) + min);
 }
 
@@ -12,16 +15,16 @@ function casuali() {
     n = getRandomNumber;
     str = "";
 
-      for (i = 0; i < n; i++) {
-           num = Math.round(Math.random()*100 + 1);
-           if (i > 0) {
-              str += ", ";
-           }
-           str += num;
-       }
+    for (i = 0; i < n; i++) {
+        num = Math.round(Math.random()*100 + 1);
+        if (i > 0) {
+            str += ", ";
+        }
+        str += num;
+    }
 
-     document.getElementById("casuali").innerHTML = "Numeri generati: " + str;
-    } 
+    document.getElementById("casuali").innerHTML = "Numeri generati: " + str;
+    
 };
 
 // stabilisco un contatore per la visualizzazione dei numeri
@@ -34,23 +37,11 @@ let clock = setInterval ( function () {
 
     if (secondi = 0) {
         clearInterval(clock);
-        alert ('buon anno nuovo');
     }else {
         secondi--;
-    }
+    };
 
-}; 1000);
-
-
-
-
-
-
-
-
-
-
-
+} *30000);
 
 //!Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //!Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
