7d88acc50a7119586156de3d8256abcf96a458a3
Chris-dagostino <dagostino.christian94@gmail.com>
2022-02-08 19:06:19 +0100

    ut

--- main.js
+++ main.js
@@ -1,47 +1,86 @@
 //!Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+//!Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+//!Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // genero dei numeri casuali e gli scrivo su HTML
 
-getRandomNumber = document.getElementById('casuali').innerHTML;
+const numeriRandom = document.getElementById('casuali').innerHTML;
+const min = 0;
+const max = 100;
+let secondi = 3;
+let clock = setInterval ( timer , 1000) 
+let messaggio = document.getElementById('risultato').innerHTML;
 
-min = 0;
-max = 100;
+ 
 
-function getRandomNumber ( min , max ) {
-    return Math.floor(Math.random()*(max - min +1) + min);
+// stabilisco un contatore per la visualizzazione dei numeri
+function timer(){
+
+    secondi = secondi -1;
+    if (secondi == 0) {
+        clearInterval(clock);
+        document.getElementById('casuali').innerHTML= "";
+        let impress = getNumeriUtente();
+    }
+    document.getElementById('contatore').innerHTML = secondi ;
+};
+
+//genero 5 numeri random
+function getRandomNumber ( x , y ) {
+    return Math.floor(Math.random()*(y - x +1) + x);
 }
 
 function casuali() {
-    n = getRandomNumber;
     str = "";
 
-    for (i = 0; i < n; i++) {
-        num = Math.round(Math.random()*100 + 1);
+    for (i = 0; i < 5; i++) {
+        num = getRandomNumber (min,max);
         if (i > 0) {
             str += ", ";
         }
         str += num;
     }
-
-    document.getElementById("casuali").innerHTML = "Numeri generati: " + str;
-    
+    document.getElementById("casuali").innerHTML = "" + str;
 };
+casuali ();
 
-// stabilisco un contatore per la visualizzazione dei numeri
+// faccio inserire per 5 volte un numero all'utente
+function getNumeriUtente() {
 
-let secondi = 30;
+    const numeriUtente = [];
 
-let clock = setInterval ( function () {
+    while (numeriUtente.length < 5) {
+        const numero = parseInt(prompt('inserisci un nmero'));
 
-    document.getElementById('contatore').innerHTML = secondi;
+        if (!numeriUtente.includes(numero) && isNaN) {
+            numeriUtente.push(numero);
+        }    
+    }
+    return numeriUtente
+}
 
-    if (secondi = 0) {
-        clearInterval(clock);
-    }else {
-        secondi--;
-    };
+// confronto i numeri inseriti con i numeri generati
+function numeriIndovinati(){
+    const numeriMatch = [];
 
-} *30000);
+    for (let i = 0; i < numeriUtente.length; i++) {
+        if (numeriRandom.includes(numeriUtente[i])) {
+            numeriMatch.push(numeriUtente);
+        }
+    }
+    return numeriMatch;
+}
 
-//!Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-//!Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+// stampo i numeri indovinati
+
+function stampoNumeriIndovinati(numeriIndovinati) {
+    const nInd = numeriIndovinati.length;
+
+    messaggio.innerHTML = `hai indovinato ${nInd} numeri ${numeriIndovinati}`;
+}
+
+
+
+
+
+
